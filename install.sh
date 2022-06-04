53539a277221b26b87a818c1ca7550b75b4085c2
thomas345 <thomas@heleneogthomas.dk>
2022-06-04 17:02:12 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,12 +6,14 @@ if [ `whoami` != 'root' ]
 fi
 apt update
 apt -y dist-upgrade
+apt -y autoremove 
 raspi-config nonint do_i2c 0
 raspi-config nonint do_ssh 0
 apt install -y git vim mc i2c-tools python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 
-pip3 install luma.oled
+pip3 install --upgrade --force-reinstall luma.oled
 pip3 install psutil
-adduser --system oled5
-usermod -a -G i2c oled5
+adduser --system oled
+usermod -a -G i2c oled
 cd /home/oled
 curl -L -o stats.py https://raw.githubusercontent.com/thomas345/carcharging-public/main/stats.py
+chown oled:users stats.py
