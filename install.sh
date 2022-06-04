c99f8ecf9e650a77e9f35ad8df6b03f04804b08a
thomas345 <thomas@heleneogthomas.dk>
2022-06-04 14:39:37 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,15 +8,11 @@ apt update
 apt dist-upgrade
 raspi-config nonint do_i2c 0
 raspi-config nonint do_ssh 0
-apt install -y python3-dev
-apt install -y python-smbus i2c-tools
-apt install -y python3-pil
-apt install -y python3-pip
-apt install -y python3-setuptools
-apt install -y python3-rpi.gpio
-apt install -y git
-git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
-cd Adafruit_Python_SSD1306
-sudo python3 setup.py install
-cd ..
+#apt install -y python3-dev python3-pil python3-pip python-smbus i2c-tools python3-setuptools python3-rpi.gpio
+apt install -y git vim mc
+cd /root/
+#git clone https://github.com/adafruit/Adafruit_Python_SSD1306.git
+#cd Adafruit_Python_SSD1306
+#python3 setup.py install
+#cd ..
 curl -L -o stats.py https://raw.githubusercontent.com/thomas345/carcharging-public/main/stats.py
