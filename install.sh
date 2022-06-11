9cfcbe8422389726ec340bd359a27e9868d372a6
thomas345 <thomas@heleneogthomas.dk>
2022-06-11 17:48:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,10 +9,6 @@ raspi-config nonint do_ssh 0
 apt update
 apt -y dist-upgrade
 apt -y autoremove 
-#echo 'jwp ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/020_jwp-nopasswd
-#echo 'tbp ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/030_tbp-nopasswd
-#useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' 'xyz') tbp
-#useradd -m -p $(perl -e 'print crypt($ARGV[0], "password")' 'xyz') jwp
 apt install -y git vim mc i2c-tools python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 
 apt install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential
 apt install -y libsdl-dev libportmidi-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
