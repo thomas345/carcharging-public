39498b410c8107bcaeefeaf79936e1efe979b9fd
thomas345 <thomas@heleneogthomas.dk>
2023-03-03 16:57:13 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ sudo apt -y dist-upgrade
 sudo apt-get update
 sudo apt-get install python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 -y
 sudo -H pip3 install luma.oled
-sudo usermod -a -G spi,gpio,i2c pi
+sudo usermod -a -G spi,gpio,i2c $USER
 #apt -y autoremove 
 #apt install -y git vim mc i2c-tools python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 
 #apt install -y python3-dev python3-pip libjpeg-dev build-essential
 #apt install -y libsdl-dev libportmidi-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
 #cd /root/
 #pip3 install --upgrade --force-reinstall luma.oled
-pip3 install psutil
+sudo pip3 install psutil
 #git clone https://github.com/rm-hull/luma.examples.git
 #cd luma.examples
 #pip3 install -e .
