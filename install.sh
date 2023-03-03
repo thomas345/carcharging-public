6931a2a6c9d28a0bc693c7b8d129823e1070bd0f
thomas345 <thomas@heleneogthomas.dk>
2023-03-03 16:47:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,17 +8,18 @@ raspi-config nonint do_i2c 0
 raspi-config nonint do_ssh 0
 apt update
 apt -y dist-upgrade
-apt -y autoremove 
+#apt -y autoremove 
 apt install -y git vim mc i2c-tools python3 python3-pip python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 
 apt install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential
 apt install -y libsdl-dev libportmidi-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
-cd /root/
+#cd /root/
 pip3 install --upgrade --force-reinstall luma.oled
 pip3 install psutil
-git clone https://github.com/rm-hull/luma.examples.git
-cd luma.examples
-pip3 install -e .
-cd examples
+#git clone https://github.com/rm-hull/luma.examples.git
+#cd luma.examples
+#pip3 install -e .
+#cd examples
 curl -L -o sys_info.py https://raw.githubusercontent.com/thomas345/carcharging-public/main/sys_info.py
-echo -e '#!/bin/sh\npython3 /root/luma.examples/examples/sys_info.py &\nexit 0' > /etc/rc.local 
+curl -L -o FreePixel.ttf https://raw.githubusercontent.com/thomas345/carcharging-public/main/FreePixel.ttf
+echo -e '#!/bin/sh\npython3 /root/sys_info.py &\nexit 0' > /etc/rc.local 
 reboot
