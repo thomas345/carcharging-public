8a74992d56e65e19ebbd902be663af2d7647ef83
thomas345 <thomas@heleneogthomas.dk>
2023-03-04 10:13:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,9 +16,6 @@ sudo usermod -a -G spi,gpio,i2c $USER
 sudo pip3 install psutil
 curl -L -o /home/$USER/sys_info.py https://raw.githubusercontent.com/thomas345/carcharging-public/main/sys_info.py?$RANDOM
 curl -L -o /home/$USER/FreePixel.ttf https://raw.githubusercontent.com/thomas345/carcharging-public/main/FreePixel.ttf?$RANDOM
-sudo su
-echo dtparam=act_led_gpio=27 >> /boot/config.txt
-echo -e '#!/bin/sh\npython3 /home/$USER/sys_info.py &\nexit 0' > /etc/rc.local 
-exit
-#reboot
+echo -e '#!/bin/sh\npython3 /home/'$USER'/sys_info.py &\nexit 0' | sudo tee /etc/rc.local
+echo dtparam=act_led_gpio=27 | sudo tee -a /boot/config.txt
 echo "Please reboot for changes to take effect"
