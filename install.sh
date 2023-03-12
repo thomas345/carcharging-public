9d821ee0318d59ecb8c9b2edd16c4c6fe4cc206f
thomas345 <thomas@heleneogthomas.dk>
2023-03-11 19:29:34 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,7 +16,13 @@ sudo usermod -a -G spi,gpio,i2c $USER
 sudo pip3 install psutil
 curl -L -o /home/$USER/sys_info.py https://raw.githubusercontent.com/thomas345/carcharging-public/main/sys_info.py?$RANDOM
 curl -L -o /home/$USER/FreePixel.ttf https://raw.githubusercontent.com/thomas345/carcharging-public/main/FreePixel.ttf?$RANDOM
-echo -e '#!/bin/sh\npython3 /home/'$USER'/sys_info.py &\nexit 0' | sudo tee /etc/rc.local
+#echo -e '#!/bin/sh\npython3 /home/'$USER'/sys_info.py &\nexit 0' | sudo tee /etc/rc.local
+crontab -l | grep '@reboot' 1>/dev/null 2>&1 
+(( $? == 0 )) && exit 
+crontab -l >/tmp/crontab.tmp 
+echo '@reboot sleep 10 && /usr/bin/python3 /home/'$USER'/sys_info.py &' >>/tmp/crontab.tmp 
+crontab /tmp/crontab.tmp 
+rm /tmp/crontab.tmp
 echo dtparam=act_led_gpio=27 | sudo tee -a /boot/config.txt
 echo dtoverlay=gpio-fan,gpiopin=4,temp=60000 | sudo tee -a /boot/config.txt
 echo "Please reboot for changes to take effect"
